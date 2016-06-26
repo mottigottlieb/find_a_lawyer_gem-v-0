0768b1ce173d54fdb9fb3369617db93a660b6c77
motti770 <mottigottlieb@gmail.com>
2016-06-26 18:38:31 +0000

    methds.rb required @ bin

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -2,28 +2,15 @@
 require 'nokogiri'
 require 'pry'
 
- class Scrape 
-        @@track=Hash.new
-        @@id=1
-         def self.create_hash(index_url)
-             site = @doc = Nokogiri::HTML(open(index_url))
-                site.search("div.lawyer-search-result").map do |info|
-                  l_name = info.search("div h3").text
-                  l_practice = info.search("p.text-truncate").text
-                  l_rating = info.search("span.text-nowrap strong").text
-                  l_years =  info.search("time").text.delete("since ").to_i
-                  l_number = info.search("span.hidden-xs").text
-               
-              time = Time.now  
-            
-              @@track[@@id]={    
-                            :name => l_name,
-                            :practice => l_practice,    
-                            :rating => l_rating,
-                            :years => time.year-l_years,
-                            :number => l_number
-                            } 
-                          @@id += 1
-                 end  
-             end  
-         end  
+ class Scrape
+
+    attr_accessor:url
+    
+    
+
+    #when  i scrape 
+        #get url 
+            #call create hash methods
+    
+     
+  end  
