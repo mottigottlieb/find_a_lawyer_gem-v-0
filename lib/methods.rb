f67c9675dc02098c5f4643669501007065d2afd9
motti770 <mottigottlieb@gmail.com>
2016-06-26 19:29:37 +0000

    all working

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,31 +1,30 @@
 class Methods
     
-    #clear hash
+    @@track=Hash.new
+    @@id=1
+     
      def self.track
           @@track
      end
      
-     	def has_number(id)
-    	    status = nil
-    	   	if @@track[id.to_i][:number].match(/[0-9]/)
-    			status = true
-    		else
-    		  status = nil
-    		end
-    		    status
-	end
+     def has_number(id)
+       status = nil
+    	if @@track[id.to_i][:number].match(/[0-9]/)
+    	  status = true
+    	else
+    	  status = nil
+    	end
+    	  status
+     end
     
-     @@track=Hash.new
-        @@id=1
-        
-         def create_hash(index_url)
-             site = @doc = Nokogiri::HTML(open(index_url))
-                site.search("div.lawyer-search-result").map do |info|
-                  l_name = info.search("div h3").text
-                  l_practice = info.search("p.text-truncate").text
-                  l_rating = info.search("span.text-nowrap strong").text
-                  l_years =  info.search("time").text.delete("since ").to_i
-                  l_number = info.search("span.hidden-xs").text
+     def create_hash(index_url)
+         site = @doc = Nokogiri::HTML(open(index_url))
+            site.search("div.lawyer-search-result").map do |info|
+              l_name = info.search("div h3").text
+              l_practice = info.search("p.text-truncate").text
+              l_rating = info.search("span.text-nowrap strong").text
+              l_years =  info.search("time").text.delete("since ").to_i
+              l_number = info.search("span.hidden-xs").text
                
               time = Time.now  
             
@@ -38,48 +37,47 @@ class Methods
                             } 
                           @@id += 1
                  end  
-             end  
+     end  
              
-             def  greet
-          puts "+------------------------------------------------+"
-          puts "|             Welcome to Find A Lawyer           |"           
-          puts "+------------------------------------------------+"
-          puts "          Legal Issue (eg: Banktrupcy)?           "
-            field = gets.strip
-          puts "          Location (Zip Code, State, City)?       "
-            location = gets.strip
-          self.create_hash("https://www.avvo.com/search/lawyer_search?utf8=%E2%9C%93&q=#{field}&loc=#{location}&button=")
-                
-                if self.class.track.empty?
-                    puts "we haven't found any info"
-                    puts "Do you want to try again?(y/n)"
-                    answer = gets.strip
-                    self.greet if answer == "y" 
-                    puts "Thank you for Visiting"
-                    exit
-                else
-                   puts "     We have found #{self.class.track.size} lawyer(s) in #{location} area      "  
-                    @@track.each do |idx, name|  
+     def greet
+       puts "+------------------------------------------------+"
+       puts "|             Welcome to Find A Lawyer           |"           
+       puts "+------------------------------------------------+"
+       puts "          Legal Issue (eg: Banktrupcy)?           "
+        field = gets.strip
+       puts "          Location (Zip Code, State, City)?       "
+        location = gets.strip
+       self.create_hash("https://www.avvo.com/search/lawyer_search?utf8=%E2%9C%93&q=#{field}&loc=#{location}&button=")
+        
+        if self.class.track.empty?
+          puts "we haven't found any info"
+          puts "Do you want to try again?(y/n)"
+          answer = gets.strip
+          self.greet if answer == "y" 
+             puts "Thank you for Visiting"
+             exit
+          else
+            puts "     We have found #{self.class.track.size} lawyer(s) in #{location} area      "  
+                 @@track.each do |idx, name|  
                    puts "              #{idx}. #{name[:name]}         "
                 end
                    self.results
                 puts "do you want to see additional options?(y/n)"       
                 options = gets.strip
                 
-               case options
-                   when "y"
-                     puts "1. see details of another lawyer"
-                     puts "2. search for other legal issue"
-                     answer = gets.strip
-                     self.results if answer == "1"
-                     self.clear_and_greet if answer =="2"
-                   else
-                      puts "Exiting the program"
-                      exit
-               end 
-                end
-    
-end
+         case options
+           when "y"
+               puts "1. see details of another lawyer"
+               puts "2. search for other legal issue"
+               answer = gets.strip
+               self.results if answer == "1"
+               self.clear_and_greet if answer =="2"
+           else
+              puts "Exiting the program"
+              exit
+         end 
+        end
+     end
 
 
        def clear_and_greet
@@ -87,15 +85,16 @@ end
            self.class.track.clear 
            self.greet
         end
+      
+        def has_phone?(id)
+             @@track[id][:number] != ""
+        end
           
- def results
+      def results
            puts "___________"
            puts "Please enter the number of lawyer you want more info on"
            chosen_lawyer = gets.strip 
            
-         def has_phone?(id)
-             @@track[id][:number] != ""
-         end
           
          
                puts <<eos 
